efc2add3521996e22c518343611b8dfbeb7f7e88
Qi-Xin <qix@andrew.cmu.edu>
2024-04-01 20:08:41 -0400

    changed 4.8e-5 to 4.8 x 10 -5

--- GLM.py
+++ GLM.py
@@ -2254,7 +2254,7 @@ def plot_function_with_excursion(V1, stationary_function, running_function, stat
     
     
     utils.use_pdf_plot(**{'axes.linewidth':0.5, 'xtick.labelsize':5, 'ytick.labelsize':5})
-    plt.subplots(6,7, figsize=(6.9,4.6), dpi=dpi)
+    plt.subplots(figsize=(6.9,4.6), dpi=dpi)
     BIGGER_SIZE = 7
     MEDIUM_SIZE = 6
     SMALL_SIZE = 5
@@ -2367,28 +2367,29 @@ def plot_function_with_excursion(V1, stationary_function, running_function, stat
                                     f'p={pvalue_toplot[function_index]:.2f}', 
                                     fontsize=SMALL_SIZE)
                         else:
-                            plt.text(0.5*function_length, 0.75*function_amp, 
+                            plt.text(0.35*function_length, 0.75*function_amp, 
                                     'p='+format_scientific_one_digit_exponent(pvalue_toplot[function_index]), 
                                     fontsize=SMALL_SIZE)
                     else:
-                        plt.text(0.5*function_length, 0.75*function_amp, 
+                        plt.text(0.4*function_length, 0.75*function_amp, 
                                  'p<'+format_scientific_one_digit_exponent(1/len(statistics_null_function[function_index])), 
                                  fontsize=SMALL_SIZE)
     # plt.tight_layout()
     
-def format_func(value):
+def format_scientific_one_digit_exponent(value):
     # make the value into a proper scientific notation
     exponent = int(np.log10(abs(value)))
     mantissa = value / 10**exponent
-    return f"{mantissa:.1f} x 10^{{{exponent}}}"
+    return f"{mantissa:.1f}$x 10^{{{exponent}}}$"
 
-def format_scientific_one_digit_exponent(number):
-    """ Formats a number in scientific notation with only one digit after 'e' """
-    formatted_number = f"{number:.1e}"
-    base, exponent = formatted_number.split('e')
-    # Removing leading '+' or '0' from the exponent part
-    formatted_exponent = exponent.replace('+0', '+').replace('-0', '-')
-    return f"${base} x 10^{formatted_exponent}$"
+# def format_scientific_one_digit_exponent(number):
+#     """ Formats a number in scientific notation with only one digit after 'e' """
+#     formatted_number = f"{number:.1e}"
+#     exponent = int(np.log10(abs(number)))
+#     base, exponent = formatted_number.split('e')
+#     # Removing leading '+' or '0' from the exponent part
+#     formatted_exponent = exponent.replace('+0', '').replace('-0', '-')
+#     return f"${base} x 10^{formatted_exponent}$"
 
 def corr(C):
     """
